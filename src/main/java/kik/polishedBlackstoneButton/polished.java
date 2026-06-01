5847c24954e11ed7bfd9ec6ccfe22f1d0f9e3a16
albe <kikmine090511@gmail.com>
2026-06-01 16:46:40 +0200

    logica per dare più item veri skibidi

--- src/main/java/kik/polishedBlackstoneButton/polished.java
+++ src/main/java/kik/polishedBlackstoneButton/polished.java
@@ -53,6 +53,31 @@ public class polished implements CommandExecutor {
             }
 
             return true;
+        } else if (args.length == 2) {
+            String arg = args[1];
+
+            try{
+                quantita = Integer.parseInt(args[2]);
+            }catch (Exception e){
+                sender.sendMessage(ChatColor.RED+"Hey! chitamortasci tua metti stocazzo di numero! \n"+e.getMessage());
+            }
+            polishedblackstonebutton.setAmount(quantita);
+            if (arg.equalsIgnoreCase("tutti")){
+                Bukkit.getServer().sendPlainMessage(sender.getName() + "vi da "+ChatColor.GOLD+quantita+ChatColor.RESET+" polished blackstone button");
+                for (Player polishedblackstonebuttoner : Bukkit.getOnlinePlayers()) {
+                    polishedblackstonebuttoner.give(polishedblackstonebutton);
+                }
+                return true;
+            }else{
+                for (Player polishedblackstonebuttoner : Bukkit.getOnlinePlayers()) {
+                    nomiplayer.add(polishedblackstonebuttoner.getName());
+                }
+                if (nomiplayer.contains(arg)){
+                    Player poraccio = Bukkit.getPlayer(arg);
+                    Bukkit.getServer().sendPlainMessage(sender.getName() + " ha dato 1 polished blackstone button a "+poraccio.getName());
+                    poraccio.give(polishedblackstonebutton);
+                }
+            }
         }
         return true;
     }
