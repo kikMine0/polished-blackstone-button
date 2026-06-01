a90dc976c004839be92310361bf1e04ff84292de
albe <kikmine090511@gmail.com>
2026-06-01 17:19:21 +0200

    bugfix

--- src/main/java/kik/polishedBlackstoneButton/polished.java
+++ src/main/java/kik/polishedBlackstoneButton/polished.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 public class polished implements CommandExecutor {
     private final Plugin plugin;
-    List<String> nomiplayer;
     int quantita;
     public polished(Plugin plugin) {
         this.plugin = plugin;
@@ -35,30 +34,28 @@ public class polished implements CommandExecutor {
 
 
         } else if (args.length == 1) {
-            String arg = args[1];
+            String arg = args[0];
             if (arg.equalsIgnoreCase("tutti")) {
                 Bukkit.getServer().sendPlainMessage(sender.getName() + "vi da polished blackstone button");
                 for (Player polishedblackstonebuttoner : Bukkit.getOnlinePlayers()) {
                     polishedblackstonebuttoner.give(polishedblackstonebutton);
                 }
             } else {
-                for (Player polishedblackstonebuttoner : Bukkit.getOnlinePlayers()) {
-                    nomiplayer.add(polishedblackstonebuttoner.getName());
-                }
-                if (nomiplayer.contains(arg)){
                     Player poraccio = Bukkit.getPlayer(arg);
+                    if (poraccio == null){sender.sendMessage(ChatColor.RED + "FACK U, METTI CIAO IUSER RIAL"); return true;}
+
                     Bukkit.getServer().sendPlainMessage(sender.getName() + " ha dato 1 polished blackstone button a "+poraccio.getName());
                     poraccio.give(polishedblackstonebutton);
-                }
+
 
             }
 
             return true;
         } else if (args.length == 2) {
-            String arg = args[1];
+            String arg = args[0];
 
             try{
-                quantita = Integer.parseInt(args[2]);
+                quantita = Integer.parseInt(args[1]);
             }catch (Exception e){
                 sender.sendMessage(ChatColor.RED+"Hey! chitamortasci tua metti stocazzo di numero! \n"+e.getMessage());
             }
@@ -70,17 +67,13 @@ public class polished implements CommandExecutor {
                 }
                 return true;
             }else{
-                for (Player polishedblackstonebuttoner : Bukkit.getOnlinePlayers()) {
-                    nomiplayer.add(polishedblackstonebuttoner.getName());
-                }
-                if (nomiplayer.contains(arg)){
+
                     Player poraccio = Bukkit.getPlayer(arg);
+                if (poraccio == null){sender.sendMessage(ChatColor.RED + "FACK U, METTI CIAO IUSER RIAL"); return true;}
                     Bukkit.getServer().sendPlainMessage(sender.getName() + " ha dato "+ChatColor.GOLD+quantita+ChatColor.RESET+" polished blackstone button a "+poraccio.getName());
                     poraccio.give(polishedblackstonebutton);
-                }else {
-                    sender.sendMessage(ChatColor.RED + "FACK U, METTI CIAO IUSER RIAL");
-                    return true;
-                }
+
+
             }
         }
 
